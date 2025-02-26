815d4dc11b3727ed5aa3183f4e39ef28c610ad8b
maska12271 <misha.zolotarjov@gmail.com>
2025-02-26 14:55:39 +0200

    commit for github pages

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/TLU_kunstiproject_2025/', // Make sure this matches your repo name
 })
