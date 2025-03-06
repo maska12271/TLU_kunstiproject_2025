15f40a48efa552ae0b30214ff4bcfb813302fb97
Drigster <62962772+Drigster@users.noreply.github.com>
2025-03-06 15:42:53 +0200

    Backend: add pagination to getUserList

--- server/trpc/routers/user.ts
+++ server/trpc/routers/user.ts
@@ -2,6 +2,7 @@ import { generateIdFromEntropySize } from "lucia";
 import { db } from "../../db";
 import { router, publicProcedure, adminProcedure } from "../trpc";
 import { z } from "zod";
+import { TRPCError } from "@trpc/server";
 
 export const userRouter = router({
   getById: adminProcedure.input(z.string()).query(async ({ input }) => {
@@ -11,17 +12,32 @@ export const userRouter = router({
       .where("id", "=", input)
       .executeTakeFirst();
   }),
-  getList: adminProcedure.query(async () => {
-    return await db.selectFrom("User").selectAll().execute();
-  }),
+  getList: adminProcedure
+    .input(
+      z.object({
+        pageNo: z.number().positive().default(1),
+        perPage: z.number().default(10),
+      }),
+    )
+    .query(async ({ input }) => {
+      const offset = (input.pageNo - 1) * input.perPage;
+      return await db
+        .selectFrom("User")
+        .selectAll()
+        .offset(offset)
+        .limit(input.perPage)
+        .execute();
+    }),
   create: adminProcedure
     .input(
       z.object({
         name: z.string(),
         isAdmin: z.boolean(),
+        projectId: z.string(),
       }),
     )
     .query(async ({ input }) => {
+      // TODO: probably shoud add check if project exists
       return await db
         .insertInto("User")
         .values({
@@ -30,8 +46,25 @@ export const userRouter = router({
           name: input.name,
           role: input.isAdmin ? "Admin" : "Member",
           password: generateIdFromEntropySize(5),
+          projectId: input.projectId,
         })
         .returningAll()
         .execute();
+    }),
+  delete: adminProcedure
+    .input(
+      z.object({
+        id: z.string(),
+      }),
+    )
+    .mutation(async ({ input }) => {
+      const result = await db
+        .deleteFrom("User")
+        .where("id", "=", input.id)
+        .execute();
+      if (result[0].numDeletedRows == 0n) {
+        throw new TRPCError({ code: "NOT_FOUND" });
+      }
+      return;
     }),
 });
